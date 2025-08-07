d0e6e2c766816ce180fbe265205efebee1380a96
Stéphane PROST-TOURNIER <stephane.pt@solution-logique.local>
2025-08-07 16:28:46 +0200

    Recup du json, mais pb affichage et de chargement de l'objet question

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,29 @@
 <template>
-  test
+  <div class="container">
+    Quiz
+    <div class="margin-top:20px">
+      {{ question.title }}
+      separation
+      <br/>
+      <br/>
+       <!--{{ question.questions[0] }}
+      <br/>
+      <br/>
+      {{ question.questions[0].choices }}-->
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { computed, onMounted, ref } from 'vue'
+
+const question = ref([])
+
+onMounted(() => {
+  const recupQuestion = fetch('../public/quiz.json')
+    .then(r => r.json())
+    .then(v => question.value = v)
+  console.log(question)
+})
+
+</script>
