214a72150cc1a58eaed31852ee4be60aa8e81d63
Stéphane PROST-TOURNIER <stephane.pt@solution-logique.local>
2025-08-08 11:26:53 +0200

    Correction - début Quiz.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <div class="container">
-    Quiz
-    <br /><br />
-   
     <div v-if="loadingState === 'error'">
       <p>
         Impossible de charger le quiz
       </p>
     </div>
     <div :aria-busy="state === 'loading'">
-       {{ quiz }}<br />
-    <br />
-    </div>
-    <div>
-      <!--{{ quiz.questions[0].question }}
-      <br />
-      <br />
-      {{ quiz.questions[0].choices }}
-      <br />
-      <br />
-      {{ quiz.questions[0].choices[1] }}-->
+      <Quiz :quiz="quiz" v-if="quiz" />
     </div>
   </div>
+
 </template>
 
 <script setup>
 import { computed, onMounted, ref } from 'vue'
+import Quiz from './components/Quiz.vue'
 
 const quiz = ref(null)
 const loadingState = ref('loading')//Pour avoir le feedback sur l'état de chargement des données
@@ -47,5 +36,10 @@ onMounted(() => {
     })
 })
 
-
 </script>
+
+<style>
+.container{
+  margin-top: 2rem;
+}
+</style>
